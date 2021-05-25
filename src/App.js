4da3fe4a0034d36c4b563a11493a842af66fdd22
jonyedson <cicerojonyedison@gmail.com>
2021-05-25 14:01:41 -0300

    last commit

--- src/App.js
+++ src/App.js
@@ -57,6 +57,13 @@ export default() => {
       <footer>
         Direitos de imagen para a netflix, dados do tmdb.org
       </footer>
+        {movieList.length <= 0 && 
+
+        <div className="loading">
+            <img src="https://media.filmelier.com/noticias/br/2020/03/Netflix_LoadTime.gif" alt="Netflix_LoadTime"></img>
+        </div>
+        
+        }
     </div>
   );
 }
