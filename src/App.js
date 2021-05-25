0b65321593e3aafdc7db655c74cc8fb56031af4f
jonyedson <cicerojonyedison@gmail.com>
2021-05-25 03:13:51 -0300

    add components Featured

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
-import FeaturedMovie from './components/FeaturedMovie';
-//1;30
+import FeaturedMovie from './components/Featured';
+import Header from './components/Header';
+
 export default() => {
 
   const [movieList, setMovieList] = useState([]);
   const [featuredData, setFeaturedData] = useState(null);
+  const [blackHeader, setBlackHeader] = useState(false);
 
   useEffect(() => {
     const loadAll = async () => {
@@ -26,9 +28,23 @@ export default() => {
 
     loadAll();
   },[]);
+  useEffect(() => {
+    const scrollListener = () => {
+      if(window.scrollY > 10){
+        setBlackHeader(true);
+      }else {
+        setBlackHeader(false);
+      }
+    }
+    window.addEventListener('scroll', scrollListener);
 
+    return () => {
+       window.removeEventListener('scroll', scrollListener);
+    }
+  }, []);
   return (
     <div className="page">
+      <Header black={blackHeader} /> 
 
       {featuredData && 
         <FeaturedMovie item={featuredData} />
@@ -37,7 +53,10 @@ export default() => {
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items}/>
         ))}
-      </section>  
+      </section>
+      <footer>
+        Direitos de imagen para a netflix, dados do tmdb.org
+      </footer>
     </div>
   );
 }
